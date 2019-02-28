e8c67abc7979e79c7ac6b438eb9b1598b2d70a07
Lucas Souza <lucas.souza@devplace.com.br>
2019-02-28 18:22:37 -0300

    Adicionado pacotes, entity framework;

--- 4 - HelloSocialNetwork.API/Program.cs
+++ 4 - HelloSocialNetwork.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace _4___HelloSocialNetwork.API
+namespace HelloSocialNetwork.API
 {
     public class Program
     {
--- 4 - HelloSocialNetwork.API/Startup.cs
+++ 4 - HelloSocialNetwork.API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace _4___HelloSocialNetwork.API
+namespace HelloSocialNetwork.API
 {
     public class Startup
     {
