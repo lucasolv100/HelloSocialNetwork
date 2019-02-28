9f53910dda0108b48d8ef0acf98b4fa97218eefc
Lucas Souza <lucas.souza@devplace.com.br>
2019-02-28 18:38:59 -0300

    Estruturado classe Usuario em Doman

---
+++ HelloSocialNetwork.Domain/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+using HelloSocialNetwork.Domain.Enums;
+
+namespace HelloSocialNetwork.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string UrlFoto { get; set; }
+        public SexoEnum Sexo { get; set; }
+
+    }
+}
---
+++ HelloSocialNetwork.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,9 @@
+namespace HelloSocialNetwork.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        Feminino = 1,
+        Masculino = 2,
+        NaoDefinido = 3
+    }
+}
