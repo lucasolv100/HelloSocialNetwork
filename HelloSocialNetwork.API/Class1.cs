db6920520f9dfaaf78dd65fc1d1b5cd236bc400e
Lucas Souza <lucas.souza@devplace.com.br>
2019-03-01 08:49:05 -0300

    Alterado estrutura do projeto via visual studio;

---
+++ HelloSocialNetwork.API/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HelloSocialNetwork.API
+{
+    public class Class1
+    {
+    }
+}
--- HelloSocialNetwork.Domain/Enums/SexoEnum.cs
+++
@@ -1,9 +0,0 @@
-namespace HelloSocialNetwork.Domain.Enums
-{
-    public enum SexoEnum
-    {
-        Feminino = 1,
-        Masculino = 2,
-        NaoDefinido = 3
-    }
-}
